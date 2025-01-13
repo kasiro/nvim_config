600366c0cad24b658a59f65d3ade8ae365a3b636
kasiro <kon.konikov@mail.ru>
2025-01-14 01:34:26 +0700

    auto upd

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -20,9 +20,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
--- emmet
-vim.g.user_emmet_leader_key = '<A-n>'
-
 -- Scroll
 vim.opt.so = 30                       -- При скролле курсор всегда по центру
 
